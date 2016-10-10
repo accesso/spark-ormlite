94c3e9405c1e96c3c6c3c3cb2c972c82cd32dfdd
Oscar Lopez <oscar.lopez@accesso.com>
2016-10-10 08:04:07 -0400

    Merge branch 'master' into feature/match_table_refactor
    Conflicts:
    	src/main/java/com/accesso/challengeladder/controller/UserController.java
    	src/main/java/com/accesso/challengeladder/services/UserService.java

--- record_match.sql
+++ record_match.sql
@@ -7,42 +7,53 @@ USE `accesso_table_tennis`$$
 
 DROP PROCEDURE IF EXISTS `record_match`$$
 
-CREATE DEFINER=`dba`@`172.16.%` PROCEDURE `record_match`(IN creatorUserId INT, IN winnerUserId INT, IN loserUserId INT, IN winnerScore INT, IN loserScore INT)
+CREATE DEFINER=`dba`@`172.16.%` PROCEDURE `record_match`(IN creatorUserId INT, IN opponentUserId INT, IN creatorScore INT, IN opponentScore INT)
 BEGIN 
-
 DECLARE match_id INT; 
-DECLARE initial_loser_rank INT; 
-DECLARE initial_winner_rank INT; 
+DECLARE winner_user_id INT; 
 DECLARE initial_creator_rank INT; 
 DECLARE initial_opponent_rank INT; 
+DECLARE dupe_match INT;
 
 # make sure it is a valid match (i.e. winner is moving up) 
-SELECT rank_id FROM ranking WHERE user_id=winnerUserId INTO initial_winner_rank; 
-SELECT rank_id FROM ranking WHERE user_id=loserUserId INTO initial_loser_rank; 
+SELECT rank_id FROM ranking WHERE user_id=creatorUserId INTO initial_creator_rank; 
+SELECT rank_id FROM ranking WHERE user_id=opponentUserId INTO initial_opponent_rank; 
 
-# not working :(
-# IF creatorUserId = winnerUserId THEN BEGIN SET initial_creator_rank=initial_winner_rank; SET initial_opponent_rank=initial_loser_rank; END; ELSEIF creatorUserId = loserUserId THEN BEGIN SET initial_creator_rank=initial_loser_rank; SET initial_opponent_rank=initial_winner_rank; END; ELSE signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid creatorUserId'; END IF; IF initial_creator_rank >= initial_opponent_rank THEN signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid match, creator is higher rank'); END IF; 
+IF initial_creator_rank >= initial_opponent_rank 
+THEN signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid match, creator is higher rank'; 
+END IF; 
+
+# dupe match check
+SELECT '1' FROM `match` WHERE creator_user_id=creatorUserId AND opponent_user_id=opponentUserId AND match_timestamp > DATE_ADD(NOW(), INTERVAL -24 HOUR) INTO dupe_match;
+IF dupe_match = '1' 
+THEN signal SQLSTATE '45000' SET MESSAGE_TEXT = 'Duplicate match'; 
+END IF;
+
+# set the winner_user_id
+SET winner_user_id = opponentUserId;
+IF creatorScore > opponentScore THEN
+BEGIN
+SET winner_user_id = creatorUserId;
+END;
+END IF; 
 
 # create the match 
-INSERT INTO `match` (match_timestamp, victor_user_id, status_id, creator_user_id) VALUES (NOW(), winnerUserId, 2, creatorUserId); 
+INSERT INTO `match` (match_timestamp, victor_user_id, status_id, creator_user_id, opponent_user_id, creator_score, opponent_score) VALUES (NOW(), winner_user_id, 2, creatorUserId, opponentUserId, creatorScore, opponentScore); 
 SET match_id = LAST_INSERT_ID(); 
-
 # match result for both players 
-INSERT INTO match_user (match_id, user_id, score) VALUES (match_id, winnerUserId, winnerScore); 
-INSERT INTO match_user (match_id, user_id, score) VALUES (match_id, loserUserId, loserScore); 
-
+#INSERT INTO match_user (match_id, user_id, score) VALUES (match_id, winnerUserId, winnerScore); 
+#INSERT INTO match_user (match_id, user_id, score) VALUES (match_id, loserUserId, loserScore); 
 # do we need to adjust rank? 
-IF winnerUserId = creatorUserId THEN 
+IF creatorScore > opponentScore THEN 
+#creator wins
 BEGIN 
-    UPDATE ranking SET user_id=winnerUserId,TIMESTAMP=NOW() WHERE rank_id=initial_loser_rank; 
-    UPDATE ranking SET user_id=loserUserId,TIMESTAMP=NOW() WHERE rank_id=initial_winner_rank; 
-
-    # add to rank history 
-    INSERT INTO ranking_history (ranking, user_id, match_id) VALUES (initial_loser_rank, winnerUserId, match_id); 
-    INSERT INTO ranking_history (ranking, user_id, match_id) VALUES (initial_winner_rank, loserUserId, match_id); 
-END; 
-END IF;
- 
+UPDATE ranking SET user_id=creatorUserId,TIMESTAMP=NOW() WHERE rank_id=initial_opponent_rank; 
+UPDATE ranking SET user_id=opponentUserId,TIMESTAMP=NOW() WHERE rank_id=initial_creator_rank; 
+# add to rank history 
+INSERT INTO ranking_history (ranking, user_id, match_id) VALUES (initial_opponent_rank, creatorUserId, match_id); 
+INSERT INTO ranking_history (ranking, user_id, match_id) VALUES (initial_creator_rank, opponentUserId, match_id); 
+END;
+END IF; 
 END$$
 
 DELIMITER ;
--- src/main/java/com/accesso/challengeladder/services/UserService.java
+++ src/main/java/com/accesso/challengeladder/services/UserService.java
@@ -10,179 +10,172 @@ import org.apache.log4j.Logger;
 import org.mindrot.jbcrypt.BCrypt;
 
 import com.accesso.challengeladder.model.Match;
+import com.accesso.challengeladder.model.MatchUser;
 import com.accesso.challengeladder.model.User;
 import com.accesso.challengeladder.utils.DBHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class UserService
 {
 
-	private static final Logger logger = Logger.getLogger(UserService.class.getCanonicalName());
+    private static final Logger logger = Logger.getLogger(UserService.class.getCanonicalName());
 
-	private ConnectionSource connectionSource;
-	private Dao<User, String> userDao;
-	private static String salt;
-	private Dao<Match, String> matchDao;
+    private ConnectionSource connectionSource;
+    private Dao<User, String> userDao;
+    private static String salt;
+    private Dao<Match, String> matchDao;
+    private Dao<MatchUser, String> matchUserDao;
 
-	public UserService() throws SQLException, IOException
-	{
-		DBHelper dBHelper = new DBHelper();
-		ConnectionSource connectionSource = dBHelper.getConnectionSource();
+    public UserService() throws SQLException, IOException
+    {
+        DBHelper dBHelper = new DBHelper();
+        ConnectionSource connectionSource = dBHelper.getConnectionSource();
 
-		this.connectionSource = connectionSource;
-		userDao = DaoManager.createDao(this.connectionSource, User.class);
+        this.connectionSource = connectionSource;
+        userDao = DaoManager.createDao(this.connectionSource, User.class);
 
-		matchDao = DaoManager.createDao(this.connectionSource, Match.class);
-	}
+        matchDao = DaoManager.createDao(this.connectionSource, Match.class);
+        matchUserDao = DaoManager.createDao(this.connectionSource, MatchUser.class);
+    }
 
-	public boolean addUser(User newUser)
-	{
-		BCrypt cryptWorker = new BCrypt();
+    public boolean addUser(User newUser)
+    {
+        BCrypt cryptWorker = new BCrypt();
 
-		try
-		{
-			salt = cryptWorker.gensalt();
+        try
+        {
+            salt = cryptWorker.gensalt();
 
-			newUser.setSalt(salt);
-			newUser.setPassword(cryptWorker.hashpw("pingpong", salt));
+            newUser.setSalt(salt);
+            newUser.setPassword(cryptWorker.hashpw("pingpong", salt));
 
-			userDao.create(newUser);
-			return true;
-		}
-		catch (Exception e)
-		{
-			logger.error("Exception..." + e.getMessage());
-			return false;
-		}
-	}
+            userDao.create(newUser);
+            return true;
+        }
+        catch (Exception e)
+        {
+            logger.error("Exception..." + e.getMessage());
+            return false;
+        }
+    }
 
-	public boolean updateUser(User updatedUser)
-	{
-		BCrypt cryptWorker = new BCrypt();
+    public boolean updateUser(User updatedUser)
+    {
+        BCrypt cryptWorker = new BCrypt();
 
-		try
-		{
-			salt = cryptWorker.gensalt();
+        try
+        {
+            salt = cryptWorker.gensalt();
 
-			Integer currentUserId = updatedUser.getId();
-			User currentUser = getUser(currentUserId.toString());
-			currentUser.setName(updatedUser.getName());
-			currentUser.setSalt(salt);
-			currentUser.setPassword(updatedUser.getPassword());
-			currentUser.setEmail(updatedUser.getEmail());
-			currentUser.setAdminFlag(updatedUser.getAdminFlag());
-			currentUser.setActiveFlag(updatedUser.getActiveFlag());
-			userDao.update(currentUser);
-			return true;
-		}
-		catch (Exception e)
-		{
-			logger.error("Exception..." + e.getMessage());
-			return false;
-		}
-	}
+            Integer currentUserId = updatedUser.getId();
+            User currentUser = getUser(currentUserId.toString());
+            currentUser.setName(updatedUser.getName());
+            currentUser.setSalt(salt);
+            currentUser.setPassword(updatedUser.getPassword());
+            currentUser.setEmail(updatedUser.getEmail());
+            currentUser.setAdminFlag(updatedUser.getAdminFlag());
+            currentUser.setActiveFlag(updatedUser.getActiveFlag());
+            userDao.update(currentUser);
+            return true;
+        }
+        catch (Exception e)
+        {
+            logger.error("Exception..." + e.getMessage());
+            return false;
+        }
+    }
 
-	public User getUser(String userId) throws SQLException, IOException
-	{
-		User user = null;
-		if (userId != null)
-		{
-			user = userDao.queryForId(userId);
-			user = populateUserWinsLosses(user);
-		}
+    public User getUser(String userId) throws SQLException
+    {
+        User user = null;
+        if (userId != null)
+        {
+            user = userDao.queryForId(userId);
+            getMatchRecord(user);
+        }
 
-		return user;
-	}
+        return user;
+    }
 
-	public User getUserData(String userId) throws SQLException
-	{
-		User user = null;
-		if (userId != null)
-		{
-			user = userDao.queryForId(userId);
-		}
+    public User getUserByEmail(String email) throws SQLException
+    {
+        Map newMap = new HashMap();
 
-		return user;
-	}
+        newMap.put("email", email);
 
-	public User getUserByEmail(String email) throws SQLException
-	{
-		Map newMap = new HashMap();
+        User user = null;
+        if (email != null)
+        {
+            List<User> results = userDao.queryForEq("email", email);
+            user = results.get(0);
+        }
+        return user;
+    }
 
-		newMap.put("email", email);
+    public String validateUser(String email, String password) throws Exception
+    {
+        BCrypt cryptWorker = new BCrypt();
 
-		User user = null;
-		if (email != null)
-		{
-			List<User> results = userDao.queryForEq("email", email);
-			user = results.get(0);
-		}
-		return user;
-	}
+        User user = getUserByEmail(email);
 
-	public String validateUser(String email, String password) throws Exception
-	{
-		BCrypt cryptWorker = new BCrypt();
+        String salt = user.getSalt();
 
-		User user = getUserByEmail(email);
+        String hashedPW = cryptWorker.hashpw(password, salt);
 
-		String salt = user.getSalt();
+        if (user.getPassword().equals(hashedPW))
+        {
+            return Integer.toString(user.getId());
+        }
 
-		String hashedPW = cryptWorker.hashpw(password, salt);
+        return null;
+    }
 
-		if (user.getPassword().equals(hashedPW))
-		{
-			return Integer.toString(user.getId());
-		}
+    public List<User> getAllUsers() throws SQLException
+    {
+        List<User> userList = userDao.queryForAll();
+        for (User user : userList)
+        {
+            user.setPassword(null);
+            user.setSalt(null);
+            user.setActiveFlag(null);
+            user.setAdminFlag(null);
+            user.setEmail(null);
+        }
+        return userList;
+    }
 
-		return null;
-	}
+    /**
+     * Queries for the match_user entries and calculates the number of wins and losses and stores them in the User object
+     *
+     * @param user
+     * @throws SQLException
+     */
+    public void getMatchRecord(User user) throws SQLException
+    {
+        QueryBuilder<MatchUser, String> matchUserQB = matchUserDao.queryBuilder();
+        matchUserQB.where().eq("user_id", user);
 
-	public List<User> getAllUsers() throws SQLException
-	{
-		List<User> userList = userDao.queryForAll();
-		return userList;
-	}
+        int numWins = 0;
+        int numLosses = 0;
 
-	/**
-	 * Queries for the match entries and calculates the number of wins and losses and stores them in the User object
-	 *
-	 * @param user
-	 * @throws SQLException
-	 * @throws IOException
-	 */
-	public User populateUserWinsLosses(User user) throws SQLException, IOException
-	{
-		// QueryBuilder<Match, String> matchQB = matchDao.queryBuilder();
-		// matchQB.where().eq("opponent_user_id", user).or().eq("creator_user_id", user);
-		// List<Match> matchList = matchQB.query();
+        List<MatchUser> matchUserList = matchUserQB.query();
+        for (MatchUser mu : matchUserList)
+        {
+            matchDao.refresh(mu.getMatch());
+            if (mu.getMatch().getVictorUser().getId() != user.getId())
+            {
+                numLosses++;
+            }
+            else
+            {
+                numWins++;
+            }
+        }
 
-		MatchService matchService = new MatchService();
-		List<Match> matchList = matchService.getMatchesByUser(user);
-
-		int numWins = 0;
-		int numLosses = 0;
-
-		for (Match match : matchList)
-		{
-			if (match.getVictorUser() != null)
-			{
-				if (match.getVictorUser().getId() != user.getId())
-				{
-					numLosses++;
-				}
-				else
-				{
-					numWins++;
-				}
-			}
-		}
-
-		user.setNumWins(numWins);
-		user.setNumLosses(numLosses);
-
-		return user;
-	}
+        user.setNumWins(numWins);
+        user.setNumLosses(numLosses);
+    }
 }
